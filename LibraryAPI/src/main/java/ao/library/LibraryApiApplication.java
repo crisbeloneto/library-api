39eadd0ddc3a482fd3516fa11aede6f650b75834
Crisbelo Neto <1000016862@ucan.edu>
2026-05-02 11:05:49 +0100

    added dockerfile

--- LibraryAPI/src/main/java/ao/library/LibraryApiApplication.java
+++ LibraryAPI/src/main/java/ao/library/LibraryApiApplication.java
@@ -2,10 +2,19 @@ package ao.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@ComponentScan(basePackages =
+{
+    "ao.library.configs",
+    "ao.library.controllers",
+    "ao.library.mappers",
+    "ao.library.repositories",
+    "ao.library.services"
+})
 @EnableJpaRepositories(basePackages = "ao.library.repositories")
+@SpringBootApplication
 public class LibraryApiApplication
 {
 
