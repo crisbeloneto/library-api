1cfb92e89e316752c60c13e182e7fd632a93d605
Crisbelo Neto <1000016862@ucan.edu>
2026-05-02 11:06:52 +0100

    changed java version to 17

--- LibraryAPI/src/main/java/ao/library/LibraryApiApplication.java
+++ LibraryAPI/src/main/java/ao/library/LibraryApiApplication.java
@@ -2,17 +2,8 @@ package ao.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan(basePackages =
-{
-    "ao.library.configs",
-    "ao.library.controllers",
-    "ao.library.mappers",
-    "ao.library.repositories",
-    "ao.library.services"
-})
 @EnableJpaRepositories(basePackages = "ao.library.repositories")
 @SpringBootApplication
 public class LibraryApiApplication
