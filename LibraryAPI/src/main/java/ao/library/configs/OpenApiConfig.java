06dd5f099e90edda52713e96f33de9f5088cf3e4
Crisbelo Neto <1000016862@ucan.edu>
2026-05-02 10:38:41 +0100

    fix: CORS and HTTPS mixed-content issue with Swagger UI

--- LibraryAPI/src/main/java/ao/library/configs/OpenApiConfig.java
+++ LibraryAPI/src/main/java/ao/library/configs/OpenApiConfig.java
@@ -8,6 +8,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,6 +25,10 @@ public class OpenApiConfig
     public OpenAPI customOpenAPI()
     {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://librarymgmtapi.onrender.com").description("Production"),
+                        new Server().url("http://localhost:8080").description("Local")
+                ))
                 .info(new Info()
                         .title("Library Management API")
                         .version("1.0")
